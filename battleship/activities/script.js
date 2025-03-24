ab3f07967bc5441702d82bda6d39868e5ea08f07
JayanaBajayna <joshuad@filamer.edu.ph>
2025-03-24 14:12:30 +0800

    whatShallWear

---
+++ battleship/activities/script.js
@@ -0,0 +1,15 @@
+function whatShallWear(temp){
+if (temp < 60) {
+    console.log("Wear your jacket Biatch");
+}
+else {
+    console.log("Wear T-Shirt Biatch");
+}
+if (temp < 70) {
+    console.log("Wear a sweater biatch");
+}
+}
+
+whatShallWear(80);
+whatShallWear(60);
+whatShallWear(50);
